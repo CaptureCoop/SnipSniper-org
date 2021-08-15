2c3088b5649ddac815d65c412e4b0669c04a06de
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-08-16 00:45:13 +0200

    Added jar uploader for github artifacts

---
+++ src/html/upload.php
@@ -0,0 +1,34 @@
+<?php
+	require("../php/uploadPassword.php");
+
+	if($_POST["password"] == $password) {
+		$filename = basename($_FILES["file"]["name"]);
+		$targetDir = "jars/";
+		$targetFile = $targetDir . $filename;
+		$filetype = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+		
+		if($_FILES["file"]["size"] > 5000000) {
+			echo("FILE TOO LARGE");
+			exit();
+		}
+		
+		if($filetype != "jar") {
+			echo("ONLY JAR");
+			exit();
+		}
+		
+		if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+			echo("SUCCESS 1/2...");
+			if(copy($targetFile, $targetDir . "SnipSniper.jar")) {
+				echo("SUCCESS 2/2");
+			} else {
+				echo("UPLOAD FAIL 2/2");
+			}
+		} else {
+			echo("UPLOAD FAIL");
+		}
+		exit();
+	}
+	
+	echo "BAD PASSWORD";
+?>
