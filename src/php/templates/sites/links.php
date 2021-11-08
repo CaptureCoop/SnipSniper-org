d6cb869ec64876e4b3c06ba403b2fbe7a3081299
SvenWollinger <wollingersven@gmail.com>
2021-11-08 13:24:12 +0100

    Changed site sourcecode link to correct url

--- src/php/templates/sites/links.php
+++ src/php/templates/sites/links.php
@@ -1,6 +1,6 @@
 <div id="content">
 	<p>SnipSniper related links:</p>
 	<a href="https://GitHub.com/CaptureCoop/SnipSniper" target="_blank">GitHub Repo</a></p>
-	<a href="https://GitHub.com/CaptureCoop/SnipSniper-org" target="_blank">This websites GitHub Repo</a></p>
+	<a href="https://GitHub.com/CaptureCoop/SnipSniper.org" target="_blank">This websites GitHub Repo</a></p>
 	<a href="https://GitHub.com/CaptureCoop/SnipSniperInstaller" target="_blank">SnipSniper Installer GitHub Repo</a></p>
 </div>
